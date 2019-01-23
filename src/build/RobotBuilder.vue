6cd5dd6425dfb756e42514de1239f5a7758978d3
Mukesh Yadav <myadav@webmsi.com>
2019-01-23 17:07:11 +0530

    Using SASS and Other CSS Pre-processors

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -68,6 +68,9 @@
 
     export default {
         name: 'RobotBuilder',
+        created(){
+          console.log('Component created');
+        },
         data() {
           return {
             cart: [],
@@ -143,15 +146,17 @@
 </script>
 
 
-<style scoped>
+<style lang="scss" scoped>
 .part {
   position: relative;
   width:165px;
   height:165px;
   border: 3px solid #aaa;
 } 
-.part img {
-  width:165px;
+.part {
+  img {
+    width:165px;
+  }
 }
 .top-row {
   display:flex;
