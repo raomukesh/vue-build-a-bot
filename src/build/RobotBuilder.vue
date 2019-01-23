e5f2b672b0eeedd25bded0f949f43fe38544d4a8
Mukesh Yadav <myadav@webmsi.com>
2019-01-23 15:20:37 +0530

    Conditionally Displaying Content with v-if and v-show

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -4,6 +4,7 @@
       <div class="top part">
         <div class="robot-name">
           {{selectedRobot.head.title}}
+          <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
         </div>
         <img :src="selectedRobot.head.src" title="head"/>
         <button @click="selectPrevHead()" class="prev-selector">&#9668;</button>
@@ -203,5 +204,8 @@
   text-align: center;
   top: -25px;
   width: 100%;
+}
+.sale{
+  color: red;
 }
 </style>
