fdf3a0768bea42bd4a9bb220904126194ea071ee
Mukesh Yadav <myadav@webmsi.com>
2019-01-24 18:47:38 +0530

    Routing from Page to Page : Working with Route Params

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="part" :class="position">
-    <img @click="showPartInfo()" :src="selectedPart.src" title="arm"/>
+    <!-- <img @click="showPartInfo()" :src="selectedPart.src" title="arm"/> -->
+    <router-link :to="{
+              name: 'Parts',
+              params: {
+                id: this.selectedPart.id,
+                partType: this.selectedPart.type,
+              },
+            }">
+      <img :src="selectedPart.src" title="arm"/>
+    </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
     <span class="sale" v-show="selectedPart.onSale">Sale!</span>
@@ -47,9 +56,15 @@ export default {
         this.emitSelectedPart();
     },
     methods: {
-        showPartInfo(){
-            this.$router.push('/parts');
-        },
+        // showPartInfo(){
+        //     this.$router.push({
+        //       name: 'Parts',
+        //       params: {
+        //         id: this.selectedPart.id,
+        //         partType: this.selectedPart.type,
+        //       },
+        //     });
+        // },
         emitSelectedPart(){
             this.$emit('partSelected', this.selectedPart);
         },
