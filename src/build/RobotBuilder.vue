83cfb9aff8b4ac04df9f3d02db611defcf167a52
Mukesh Yadav <myadav@webmsi.com>
2019-01-29 13:57:03 +0530

    Returning Promises from Actions

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -108,7 +108,8 @@
               robot.torso.cost +
               robot.rightArm.cost +
               robot.base.cost;
-            this.$store.dispatch('addRobotToCart', Object.assign({}, robot, {cost}));
+            this.$store.dispatch('addRobotToCart', Object.assign({}, robot, { cost }))
+              .then(() => this.$router.push('/cart'));
             this.addedToCart = true;
           },
         },
