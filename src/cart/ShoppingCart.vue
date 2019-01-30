117e3060d8213e3079cd6eeef15b1d460cc2f454
Mukesh Yadav <myadav@webmsi.com>
2019-01-30 13:44:51 +0530

    M7 : Declaring Filters Globally

--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -51,13 +51,8 @@
 </template>
 
 <script>
-import currencyFilter from '../shared/currency-filter';
-
 export default {
   name: 'Cart',
-  filters: {
-    currency: currencyFilter
-  },
   computed: {
       cart() {
           return this.$store.state.robots.cart; 
