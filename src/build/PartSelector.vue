675166db0c30ae04b414f662ac12cf8e3f13b2d8
Mukesh Yadav <myadav@webmsi.com>
2019-01-30 12:30:08 +0530

    M7 : Passing Data to Directives

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -12,7 +12,7 @@
     </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span v-pin class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span v-pin="{ bottom: '10px', right: '5px'}" class="sale" v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
